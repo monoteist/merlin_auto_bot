4dc5f8c57f5555051e01b63406f8a5879a1b941b
monoteist <i.ezerbaev97@gmail.com>
2026-02-21 22:05:58 +0300

    update chat

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 import telebot
 from merlin import get_cars, get_url
 
-API_TOKEN = '968973363:AAFUWqxeWI_7iPz0mu4zUltiHahATemu_wU'
+API_TOKEN = '8288614878:AAH-3jibTIzCxEiA7JbQKFUlruSmnuAnaWU'
 bot = telebot.TeleBot(API_TOKEN)
 
 
 @bot.message_handler(commands=['run'])
 def start(message):
     print("working")
-    auction_chat_id = 156649256
+    auction_chat_id = 518129651
     # url = 'https://www.merlin.ie/stock/?culture=en-GB&locationid=36&saleid=482&excludeClosed=true&page=3&pagesize=74&sortby=reference_asc'
     url = get_url()
     get_cars(url, auction_chat_id, bot)
